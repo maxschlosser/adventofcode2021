19d9b514cba0a65ca6c6a4cdf0b4c233c31cfaad
Maximilian Schlosser <maxschlosser96@googlemail.com>
2021-12-02 00:36:18 +0100

    solution first day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,19 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.zipWithNext { current, next -> if (current >= next) 0 else 1 }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        val inputWindowSum = input.windowed(3).map { it.sum() }
+        return part1(inputWindowSum)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map{ it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map{ it.toInt() }
     println(part1(input))
     println(part2(input))
 }
